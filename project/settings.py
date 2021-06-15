a7109c68cdbd82973d5386ce20a59deca244c1f3
ashleshshenoy <ashleshshenoy09@gmail.com>
2021-06-15 19:15:19 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -147,7 +147,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
@@ -156,5 +155,4 @@ STATICFILES_DIRS = (
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEBUG_COLLECTSTATIC=1
 DISABLE_COLLECTSTATIC=1
