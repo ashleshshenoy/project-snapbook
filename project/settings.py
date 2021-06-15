d876300f556bdceb004eb65c1b7e32187a51dfd3
ashleshshenoy <ashleshshenoy09@gmail.com>
2021-06-15 19:12:54 +0530

    static file update

--- project/settings.py
+++ project/settings.py
@@ -139,3 +139,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='front-page'
 LOGIN_URL='login'
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEBUG_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC=1
